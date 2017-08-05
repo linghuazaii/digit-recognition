d00a262b16c91eb2b3b684bd8388806b21eb97d7
linghuazaii <lsh@kloudpeak.com>
2017-08-05 10:42:53 +0000

    m

--- neural-networks.py
+++ neural-networks.py
@@ -14,6 +14,9 @@ def sigmoid(z):
             val[x][y] = 0.
     return val
 
+def sigmoid_derivative(z):
+    return sigmoid(z) * (1. - sigmoid(z))
+
 class NeuralNetwork(object):
     def __init__(self, sizes):
         self.num_layers = len(sizes)
@@ -41,11 +44,51 @@ class NeuralNetwork(object):
             random.shuffle(training_data)
             mini_batches = [training_data[k: k + mini_batch_size] for k in xrange(0, n, mini_batch_size)]
             for mini_batch in mini_batches:
-                self.update_mini_batch(mini_batch, eta)
+                self.updateMiniBatch(mini_batch, eta)
             if test_data:
                 print 'Epoch %s: %s / %s' % (j, self.evaluate(test_data), n_test)
             else:
                 print 'Epoch %s complete.' % j
+
+    def updateMiniBatch(self, mini_batch, eta):
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        for x, y in mini_batch:
+            delta_nable_b, delta_nabla_w = self.backprob(x, y)
+            nabla_b = [nb + dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+            nabla_w = [nw + dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+        self.weights = [w - (eta / len(mini_batch)) * nw for w, nw in zip(self.weights, nabla_w)]
+        self.biases = [b - (eta / len(mini_batch)) * nb for b, nb in zip(self.biases, nabla_b)]
+
+    def backprob(self, x, y):
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+
+        activation = x
+        activations = [x]
+        zs = []
+        for b, w in zip(self.biases, self.weights):
+            z = np.dot(w, activation) + b
+            zs.append(z)
+            activation = sigmoid(z)
+            activations.append(activation)
+        delta = self.cost_derivative(activations[-1], y) * sigmoid_prime(zs[-1])
+        nabla_b[-1] = delta
+        nabla_w[-1] = np.dot(delta, activations[-2].transpose())
+        for l in xrange(2, self.num_layers):
+            z = zs[-l]
+            sd = sigmoid_derivative(z)
+            delta = np.dot(self.weight[-l + 1].transpose(), delta) * sd
+            nabla_b[-l] = delta
+            nabla_w[-l] = np.dot(delta, activations[-l - 1].transpose())
+        return (nabla_a, nabla_w)
+
+    def evaluate(self, test_data):
+        test_result = [(np.argmax(self.feedForward(x)), y) for (x, y) in test_data]
+        return sum(int(x == y) for (x, y) in test_results)
+
+    def cost_derivative(self, output_activations, y):
+        return (output_activations - y)
 
 def main():
     nn = NeuralNetwork([5,8,4,2,1])
